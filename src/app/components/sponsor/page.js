631b867f546f33ba038920a3fb6a7fa469115ab9
Elgara5364 <steveloardi5364@gmail.com>
2024-04-01 16:28:04 +0900

    add mx on Sponsor section

--- src/app/components/sponsor/page.js
+++ src/app/components/sponsor/page.js
@@ -6,9 +6,9 @@ import Youtube from "/public/img/sponsor4.png";
 
 const Sponsor = () => {
   return (
-    <section className="pt-16 pb-8 ">
-      <div className="container mx-auto">
-        <div className="grid grid-cols-4 gap-y-16 place-items-center max-[600px]:grid-cols-auto-fill-120">
+    <section className="pt-16 pb-8 lg:w-[968px] mx-auto">
+      <div className="container  lg:mx-auto">
+        <div className="grid grid-cols-4 mx-5 gap-y-16 place-items-center max-[600px]:grid-cols-auto-fill-120 ">
           <Image
             src={Apple}
             width={90}
